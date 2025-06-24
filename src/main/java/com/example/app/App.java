b57a2848c5cd81a2bbd2eb54ad343d816cf16004
Techkers <parthsharma079@gmail.com>
2025-06-24 19:42:54 +0530

    Initial commit - Java Mavan App

---
+++ src/main/java/com/example/app/App.java
@@ -0,0 +1,7 @@
+package com.example.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Parth! This is your first Maven project.");
+    }
+}
